5831a087534ea6b7cc657ce99ccf8eb6e004001a
Huy Do <huyddo40@gmail.com>
2019-05-22 15:23:44 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,13 +1,13 @@
 def using_while
 	levitation_force = 6
 	while levitation_force < 10
-<<<<<<< HEAD
+
   	puts "Wingardium Leviosa"
   	levitation_force += 1
-=======
+
   	puts "#{levitation_force}"
   	evitation_force += 1
->>>>>>> 4d42d4d8fa2c4f8db35a2a3daa2258f9805d006f
+
 	end
 end
 
